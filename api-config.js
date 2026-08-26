0427363fc7601fcaad9d1bae2df05606d891c8c8
lainyoffc <lainy52twitch@gmail.com>
2026-08-26 13:06:08 +0300

    Add reviews API configuration

---
+++ api-config.js
@@ -0,0 +1,3 @@
+// Адрес Render Web Service с API отзывов.
+// После создания сервиса проверь, что URL совпадает с адресом ниже.
+window.REVIEWS_API_URL = 'https://f3hcqcx-reviews-api.onrender.com';
